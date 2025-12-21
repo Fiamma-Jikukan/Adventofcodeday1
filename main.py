4fa1f848da97252d1c7e3b0bb726daf1ba776d72
Fiamma <stavtxxx1@gmail.com>
2025-12-21 12:14:37 +0200

    day 8 continue

--- main.py
+++ main.py
@@ -293,6 +293,201 @@ def remove_papers(matrix_of_papers, removable_papers):
     return matrix_of_papers
 
 
+def split_into_ingredients_ranges_and_ids(ingredients):
+    place_of_split = 0
+    for index in range(len(ingredients)):
+        if ingredients[index] == '':
+            place_of_split = index
+            break
+    ranges = ingredients[:place_of_split]
+    ids = ingredients[place_of_split + 1:]
+    return ranges, ids
+
+
+def str_range_to_top_and_bottom(range_str):
+    return range_str.split('-')
+
+
+def is_id_in_range(id, range):
+    top_bottom_str = str_range_to_top_and_bottom(range)
+    bottom = int(top_bottom_str[0])
+    top = int(top_bottom_str[1])
+    id_int = int(id)
+    if bottom <= id_int <= top:
+        return True
+    return False
+
+
+def is_id_in_any_range(id, ranges):
+    for range_str in ranges:
+        if is_id_in_range(id, range_str):
+            return True
+    return False
+
+
+def how_many_ids_are_fresh(ranges):
+    top_bottom_str = str_range_to_top_and_bottom(ranges)
+    bottom = int(top_bottom_str[0])
+    top = int(top_bottom_str[1])
+    return top - bottom + 1
+
+
+def add_ids_to_list(list_of_fresh_ids, range_str):
+    top_bottom_str = str_range_to_top_and_bottom(range_str)
+    bottom = int(top_bottom_str[0])
+    top = int(top_bottom_str[1])
+    for num in range(bottom, top + 1):
+        if num not in list_of_fresh_ids:
+            list_of_fresh_ids.append(num)
+    list_of_fresh_ids.sort()
+    return list_of_fresh_ids
+
+
+def convert_str_ranges_to_int_ranges(str_ranges):
+    int_ranges = []
+    for range_strings in str_ranges:
+        top_bottom_str = str_range_to_top_and_bottom(range_strings)
+        bottom = int(top_bottom_str[0])
+        top = int(top_bottom_str[1])
+        int_ranges.append([bottom, top])
+    return int_ranges
+
+
+def add_range_to_list(all_fresh_ranges, curr_range):
+    bottom = curr_range[0]
+    top = curr_range[1]
+    if not all_fresh_ranges:
+        bottom_top = [bottom, top]
+        all_fresh_ranges.append(bottom_top)
+        return all_fresh_ranges
+
+    for i in range(len(all_fresh_ranges)):
+        # case 1: entire range is within them
+        if (all_fresh_ranges[i][0] <= bottom <= all_fresh_ranges[i][1]) and (
+                all_fresh_ranges[i][0] <= top <= all_fresh_ranges[i][1]):
+            return all_fresh_ranges
+
+        # case 2: bottom is within an existing range, top is outside of it
+        if all_fresh_ranges[i][0] <= bottom <= all_fresh_ranges[i][1]:
+            all_fresh_ranges[i][1] = top
+            return all_fresh_ranges
+
+        # case 3: top is within range, bottom is outside
+        if all_fresh_ranges[i][0] <= top <= all_fresh_ranges[i][1]:
+            all_fresh_ranges[i][0] = bottom
+            return all_fresh_ranges
+
+    bottom_top = [bottom, top]
+    all_fresh_ranges.append(bottom_top)
+    return all_fresh_ranges
+
+
+def sum_lines_form_worksheet(full_worksheet):
+    length_of_worksheet = len(full_worksheet[0])
+    result = 0
+    for i in range(length_of_worksheet):
+        num1 = int(full_worksheet[0][i])
+        num2 = int(full_worksheet[1][i])
+        num3 = int(full_worksheet[2][i])
+        num4 = int(full_worksheet[3][i])
+        if full_worksheet[-1][i] == '+':
+            result += (num1 + num2 + num3 + num4)
+
+        if full_worksheet[-1][i] == '*':
+            result += (num1 * num2 * num3 * num4)
+
+    return result
+
+
+def order_sheet_by_numbers(full_worksheet):
+    length_of_worksheet = len(full_worksheet[0])
+    how_many_lines = len(full_worksheet)
+    result = []
+    for i in range(length_of_worksheet):
+        column = []
+        for j in range(how_many_lines):
+            print(full_worksheet[j][i])
+            column.append(full_worksheet[j][i])
+        print(column)
+        result.append(column)
+    return result
+
+
+def create_operator_arr(operators_str):
+    str_to_arr = operators_str.split(' ')
+    result = [i for i in str_to_arr if i != '']
+    return result
+
+
+def calc_line_with_operator(worksheet_full, operator_list):
+    ""
+    print(worksheet_full)
+    print(operator_list)
+    operator_list_index = 0
+    sum_of_each_line_by_index = []
+
+    for i in range(len(worksheet_full[0])):
+        num_list = []
+        empty_counter = 0
+        operator = ''
+        if operator_list[operator_list_index] == '+':
+            operator = '+'
+        if operator_list[operator_list_index] == '*':
+            operator = '*'
+
+        for j in range(len(worksheet_full)):
+            if worksheet_full[j][i] == ' ':
+                empty_counter += 1
+            else:
+                num_list.append(worksheet_full[j][i])
+
+        if empty_counter == len(worksheet_full):
+            operator_list_index += 1
+            continue
+        print(num_list)
+        num_to_int = int(''.join(num_list))
+        print(num_to_int)
+        if len(sum_of_each_line_by_index) <= operator_list_index:
+            sum_of_each_line_by_index.append(num_to_int)
+            continue
+        # if len(sum_of_each_line_by_index) == 0:
+        #     sum_of_each_line_by_index.append(num_to_int)
+        #     continue
+        if operator == '+':
+            sum_of_each_line_by_index[operator_list_index] += int(num_to_int)
+        if operator == '*':
+            sum_of_each_line_by_index[operator_list_index] *= int(num_to_int)
+    return sum_of_each_line_by_index
+
+
+def teleporter_activate(teleporter_list):
+    num_of_splits = 0
+    for i in range(len(teleporter_list)):
+        if i == 0:
+            for j in range(len(teleporter_list[0])):
+                if teleporter_list[0][j] == 'S':
+                    teleporter_list[1][j] = '|'
+
+        if i != len(teleporter_list) - 2:
+            for j in range(len(teleporter_list[i])):
+                if teleporter_list[i][j] == '|' and teleporter_list[i + 1][j] == '^':
+                    # if teleporter_list[i + 1][j - 1] == '|':
+                    #     num_of_splits -= 1
+                    # if teleporter_list[i + 1][j + 1] == '|':
+                    #     num_of_splits -= 1
+
+                    teleporter_list[i + 1][j - 1] = '|'
+                    teleporter_list[i + 1][j + 1] = '|'
+                    num_of_splits += 2
+
+                if teleporter_list[i][j] == '|' and teleporter_list[i + 1][j] == '.':
+                    teleporter_list[i + 1][j] = '|'
+
+        print(teleporter_list[i])
+
+    return num_of_splits
+
+
 if __name__ == '__main__':
     # rotations = read_file_to_list("input_rotations.txt")
     # num_of_rotations = len(rotations)
@@ -305,7 +500,7 @@ if __name__ == '__main__':
     #
     # print(dial)
     # print("num of zeros is: ", num_of_zeros)
-
+    #
     # print("part 2")
     # ids = read_ranges_to_list("input_ids.txt")
     # print(ids)
@@ -316,7 +511,7 @@ if __name__ == '__main__':
     #         sum += invalids[j]
     #
     # print(sum)
-
+    #
     # print("part 3")
     # banks = read_file_to_list("banks_input.txt")
     # sum = 0
@@ -328,25 +523,117 @@ if __name__ == '__main__':
     #     print(batteries_to_activate)
     #
     # print("sum of batteries:", sum, "\n")
+    #
+    # print("part 4")
+    # papers = read_file_to_list("input_papers.txt")
+    # papers_matrix = []
+    # for paper in papers:
+    #     papers_matrix.append(divide_str(paper, 1))
+    #
+    # for paper_roll in papers_matrix:
+    #     print(paper_roll)
+    #
+    # total_removed = 0
+    # while True:
+    #     how_many_accessible = find_out_how_many_papers_accessible(papers_matrix)[0]
+    #     if how_many_accessible == 0:
+    #         break
+    #     removables = find_out_how_many_papers_accessible(papers_matrix)[1]
+    #     removed_papers_metrix = remove_papers(papers_matrix, removables)
+    #     papers_matrix = removed_papers_metrix
+    #     total_removed += how_many_accessible
+    # print("total_removed:", total_removed)
+    #
+    # print("day 5")
+    # ingredients = read_file_to_list("input_ingredients.txt")
+    # all_ranges, ids = split_into_ingredients_ranges_and_ids(ingredients)
+    # all_ranges.sort()
+    # print(all_ranges)
+    #
+    # # sum_of_fresh_ingredients = 0
+    # # for id in ids:
+    # #     if is_id_in_any_range(id, all_ranges):
+    # #         sum_of_fresh_ingredients += 1
+    # # print(sum_of_fresh_ingredients)
+    #
+    # sum_of_fresh_ids = 0
+    # all_ranges_int = convert_str_ranges_to_int_ranges(all_ranges)
+    # fresh_ids = []
+    # for range_int in all_ranges_int:
+    #     fresh_ids = add_range_to_list(fresh_ids, range_int)
+    #
+    # new_fresh_ids = []
+    # while True:
+    #
+    #     first_len = len(new_fresh_ids)
+    #     for range_num in fresh_ids:
+    #         new_fresh_ids = add_range_to_list(new_fresh_ids, range_num)
+    #     second_len = len(new_fresh_ids)
+    #     if first_len == second_len:
+    #         print("from while:", fresh_ids)
+    #         print("first and second len:", first_len, second_len)
+    #         fresh_ids = new_fresh_ids
+    #         break
+    #     fresh_ids = new_fresh_ids
+    #
+    #
+    #
+    # # print(fresh_ids)
+    #
+    # for range_int in fresh_ids:
+    #     sum_of_fresh_ids += range_int[1] - range_int[0] + 1
+    #
+    # print(sum_of_fresh_ids)
 
-    print("part 4")
-    papers = read_file_to_list("input_papers.txt")
-    papers_matrix = []
-    for paper in papers:
-        papers_matrix.append(divide_str(paper, 1))
+    # print("day 6")
+    # worksheet = read_file_to_list("input_worksheet.txt")
+    # # print(worksheet)
+    # for i in range(len(worksheet)):
+    #     if worksheet[i][-1] == '\n':
+    #         worksheet[i] = worksheet[i][:-1]
+    # # print(worksheet)
+    # operator_list = create_operator_arr(worksheet[-1])
+    # # print(operator_list)
+    # worksheet_solution = calc_line_with_operator(worksheet[:-1], operator_list)
+    # print(worksheet_solution)
+    # print(sum(worksheet_solution))
+    #
+    # # worksheets_lines = []
+    # # for i in range(4):
+    # #     line = worksheet[i].split(' ')
+    # #     line_without_space = []
+    # #     for element in line:
+    # #         if element != '':
+    # #             line_without_space.append(element)
+    # #     worksheets_lines.append(line_without_space)
+    # # for element in worksheets_lines:
+    # #     print(element)
+    # #
+    # # reshaped_worksheet = order_sheet_by_numbers(worksheets_lines)
+    # # print(reshaped_worksheet)
+    #
+    # # sum_of_all_worksheet = sum_lines_form_worksheet(worksheets_lines)
+    # # print(sum_of_all_worksheet)
 
-    for paper_roll in papers_matrix:
-        print(paper_roll)
+    # print("day 7")
+    # teleporter_str = read_file_to_list("input_teleporter_copy.txt")
+    # teleporter = []
+    # for i in range(len(teleporter_str)):
+    #     teleporter.append(list(teleporter_str[i]))
+    #
+    # for line in teleporter:
+    #     print(line)
+    # s_index = 0
+    # for j in range(len(teleporter[0])):
+    #     if teleporter[0][j] == 'S':
+    #         s_index += 1
+    #
+    #
+    # print("\n")
+    # splits = teleporter_activate(teleporter)
+    # print(splits)
+    #
+    # for line in teleporter:
+    #     print(line)
 
-    total_removed = 0
-    while True:
-        how_many_accessible = find_out_how_many_papers_accessible(papers_matrix)[0]
-        if how_many_accessible == 0:
-            break
-        removables = find_out_how_many_papers_accessible(papers_matrix)[1]
-        removed_papers_metrix = remove_papers(papers_matrix, removables)
-        papers_matrix = removed_papers_metrix
-        total_removed += how_many_accessible
-    print("total_removed:", total_removed)
-
-
+    print("day 8")
